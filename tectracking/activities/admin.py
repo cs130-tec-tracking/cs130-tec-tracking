eaf45f3b61f07f54e17a6b9ad78ef81ea6eb8ea5
jalaziz <me@jalaziz.net>
2011-05-06 02:00:56 -0700

    Implemented Activity Tracking app models, including the associated admin objects.
    closes #4

---
+++ tectracking/activities/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+from models import Activity, Room, RoomReservation, StatusType, Task, ActivityTask, Assignment, Note
+from django.contrib.admin.templatetags.admin_list import date_hierarchy
+
+class RoomReservationInline(admin.StackedInline):
+    model = RoomReservation
+    extra = 1
+
+class RoomReservationTabularInline(admin.TabularInline):
+    model = RoomReservation
+    extra = 1
+
+class ActivityTaskInline(admin.TabularInline):
+    model = ActivityTask
+
+class AssignmentInline(admin.StackedInline):
+    model = Assignment
+    max_num = 1
+
+class NoteInline(admin.TabularInline):
+    model = Note
+    extra = 1
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('tec_id', 'event_type', 'short_desc', 'event_start_date', 'siebel_num', 'num_attendees',)
+    list_filter = ('assignment__user', 'event_type', 'tec_site', 'num_attendees', 'room_required', 'laptops_required',)
+    search_fields = ('assignment__user__first_name', 'assignment__user__last_name', 'siebel_num', 'event_organizer', 'event_type', 'tec_site',)
+    inlines = (AssignmentInline, ActivityTaskInline, NoteInline, RoomReservationInline,)
+    date_hierarchy = 'event_start_date'
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('location', 'capacity', 'num_workstations',)
+    list_filter = ('capacity', 'num_workstations',)
+    search_fields = ('location',)
+    inlines = (RoomReservationTabularInline,)
+
+class RoomReservationAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'room', 'datetime', 'duration',)
+    list_filter = ('room', 'datetime', 'duration',)
+    search_fields = ('activity__tec_id', 'room__location',)
+    date_hierarchy = 'datetime'
+
+class StatusTypeAdmin(admin.ModelAdmin):
+    pass
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status_type',)
+    inlines = (ActivityTaskInline,)
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'user', 'priority', 'escalation_status',)
+    list_filter = ('user', 'priority',)
+    search_fields = ('user__first_name', 'user__last_name', 'activity__tec_id',)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'user', 'message',)
+    list_filter = ('user',)
+    search_fields = ('user__first_name', 'user__last_name', 'activity__tec_id',)
+
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(RoomReservation, RoomReservationAdmin)
+admin.site.register(StatusType, StatusTypeAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Assignment, AssignmentAdmin)
+admin.site.register(Note, NoteAdmin)
--- tectracking/activities/models.py
+++ tectracking/activities/models.py
@@ -1,3 +1,108 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Activity(models.Model):
+    REQUIRED_CHOICES = (
+        (u'Y', u'Yes'),
+        (u'N', u'No'),
+    )
+
+    tec_id = models.CharField(max_length=11, primary_key=True)
+    event_type = models.CharField(max_length=10)
+    short_desc = models.CharField(max_length=60, verbose_name='short description')
+    reservation_desc = models.CharField(max_length=300, verbose_name='reservation description')
+    tec_site = models.CharField(max_length=60)
+    event_location = models.CharField(max_length=60)
+    event_start_date = models.DateField()
+    event_end_date = models.DateField()
+    event_organizer = models.CharField(max_length=60)
+    date_entered = models.DateField()
+    date_last_mod = models.DateField(null=True, blank=True, verbose_name='date last modified')
+    event_size = models.IntegerField(null=True, blank=True)
+    sponsor_mgr = models.CharField(max_length=60, null=True, blank=True, verbose_name='sponsor manager')
+    class_mgr = models.CharField(max_length=60, verbose_name='class manager')
+    siebel_num = models.CharField(max_length=9, null=True, blank=True, db_column='siebel_no', verbose_name='siebel number')
+    num_attendees = models.IntegerField(db_column='no_attendees', verbose_name='number of attendees')
+    room_required = models.CharField(max_length=1, choices=REQUIRED_CHOICES)
+    laptops_required = models.CharField(max_length=1, choices=REQUIRED_CHOICES)
+
+    reserved_rooms = models.ManyToManyField('Room', through='RoomReservation')
+    tasks = models.ManyToManyField('Task', through='ActivityTask')
+
+    class Meta:
+        db_table = 'tracking_source'
+        verbose_name_plural = 'activities'
+        ordering = ['-event_start_date', '-event_end_date']
+
+    def __unicode__(self):
+        return self.tec_id
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=300)
+    capacity = models.IntegerField()
+    num_workstations = models.IntegerField(null=True, blank=True, db_column='no_workstations', verbose_name='number of worksations')
+
+    class Meta:
+        db_table = 'room'
+
+    def __unicode__(self):
+        return self.location
+
+class RoomReservation(models.Model):
+    activity = models.ForeignKey(Activity, db_column='tec_id')
+    room = models.ForeignKey(Room)
+    datetime = models.DateTimeField(verbose_name='time')
+    duration = models.TimeField()
+
+    class Meta:
+        db_table = 'room_reservations'
+        ordering = ['-datetime', '-duration']
+
+class StatusType(models.Model):
+    status_type_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        db_table = 'status_type'
+
+    def __unicode__(self):
+        return self.name
+
+class Task(models.Model):
+    task_id = models.AutoField(primary_key=True)
+    status_type = models.ForeignKey(StatusType, db_column='status_type_id')
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        db_table = 'task'
+
+    def __unicode__(self):
+        return self.name
+
+class ActivityTask(models.Model):
+    activity = models.ForeignKey(Activity, db_column='tec_id')
+    task = models.ForeignKey(Task)
+    status = models.CharField(max_length=300, null=True, blank=True)
+
+    class Meta:
+        db_table = 'activity_tasks'
+
+class Assignment(models.Model):
+    activity = models.OneToOneField(Activity, db_column='tec_id')
+    user = models.ForeignKey(User, db_column='auth_user_id')
+    priority = models.SmallIntegerField()
+    escalation_status = models.CharField(max_length=300, null=True, blank=True)
+
+    class Meta:
+        db_table = 'activity_assignments'
+        ordering = ['priority']
+
+class Note(models.Model):
+    note_id = models.AutoField(primary_key=True)
+    activity = models.ForeignKey(Activity, db_column='tec_id')
+    user = models.ForeignKey(User, db_column='auth_user_id')
+    message = models.TextField()
+
+    class Meta:
+        db_table = 'activity_note'
