77d89c46c462756edde2b516b9d0b8e8ad4f9001
jalaziz <me@jalaziz.net>
2011-05-10 02:38:54 -0700

    Added activity detail and tasks views

--- tectracking/activities/urls.py
+++ tectracking/activities/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
-from tectracking.activities.views import ActivityListView, CalendarView
+from tectracking.activities.views import ActivityListView, ActivityDetailView, CalendarView, \
+    ActivityTaskListView
 
 urlpatterns = patterns(
     '',
     (r'^$', ActivityListView.as_view()),
     (r'^calendar/$', CalendarView.as_view()),
+    (r'^(?P<pk>.{11})/$', ActivityDetailView.as_view()),
+    (r'^(?P<pk>.{11})/tasks/$', ActivityTaskListView.as_view())
 )
 
--- tectracking/activities/views.py
+++ tectracking/activities/views.py
@@ -1,9 +1,25 @@
-from models import Activity
+from models import Activity, ActivityTask
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.shortcuts import get_list_or_404
 
 class CalendarView(TemplateView):
     template_name = 'calendar.html'
 
 class ActivityListView(ListView):
     model = Activity
+
+class ActivityDetailView(DetailView):
+    model = Activity
+
+class ActivityTaskListView(ListView):
+    model = ActivityTask
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk', None)
+
+        if pk is not None:
+            return ActivityTask.objects.filter(activity__pk=pk)
+
+        return None
