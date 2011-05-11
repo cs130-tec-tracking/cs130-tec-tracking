31bfbc6daa51fc3cf3198adb73c021e821815267
jalaziz <me@jalaziz.net>
2011-05-11 15:15:11 -0700

    Added an index view
    The index view currently displays the base template. More will be added later.

--- tectracking/common/views.py
+++ tectracking/common/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'base.html'
--- tectracking/urls.py
+++ tectracking/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.base import RedirectView
+from tectracking.common.views import IndexView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='/index')),
+    url(r'^index/$', IndexView.as_view()),
     url(r'^activities/', include('tectracking.activities.urls')),
     url(r'^inventory/', include('tectracking.inventory.urls')),
 
