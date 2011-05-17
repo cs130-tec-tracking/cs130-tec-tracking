14f7ea905fc0d90c8a47c01bb261685ef44323cb
Mikal <mikal@saltve.it>
2011-05-17 01:45:56 -0700

    Major changes to style.css, base.html, activity_list.html and script.js -Mikal

--- tectracking/common/static/js/script.js
+++ tectracking/common/static/js/script.js
@@ -2,8 +2,11 @@
 */
 
 $("nav").buttonset();
+/*$("th").button();*/
+/*$(".activity_header_button").button();*/
 $(".jsbutton").button();
-$("#authlinks .login").button();
+/*$("#authlinks .login").button();*/
+$(".linkbutton").button();
 
 
 
