25cf9381c24ccbc06a551232d504a2dc2d27c994
Mikal <mikal@saltve.it>
2011-05-17 02:24:31 -0700

    Added styling to the table headers.  jQuery functionality needs to be added to allow filtering/sorting and also to highlight the column being sorted on.

--- tectracking/common/static/js/script.js
+++ tectracking/common/static/js/script.js
@@ -3,7 +3,12 @@
 
 $("nav").buttonset();
 /*$("th").button();*/
-/*$(".activity_header_button").button();*/
+$(".activity_header_button").button({
+
+	icons: {secondary:'ui-icon-triangle-1-s'}
+});
+
+/*$("#radio1").buttonset();*/
 $(".jsbutton").button();
 /*$("#authlinks .login").button();*/
 $(".linkbutton").button();
