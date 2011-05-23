a6f5320e210be5b21567e9957d5e250aa0bd70e1
jalaziz <me@jalaziz.net>
2011-05-22 18:44:03 -0700

    The activity list page now uses the activity_list context variable that is passed automatically

--- tectracking/activities/views.py
+++ tectracking/activities/views.py
@@ -12,8 +12,6 @@ class ActivityListView(ListView):
     model = Activity
 
     def get_context_data(self, **kwargs):
-        activities = Activity.objects.all()
-
         if self.request.user.is_authenticated():
             assignments = Assignment.objects.filter(user=self.request.user)
         else:
@@ -30,7 +28,6 @@ class ActivityListView(ListView):
             'assignments': assignments,
             'unassigned_activities': unassigned_activities,
             'users': users,
-            'activities': activities,
         }
         kwargs.update(context)
         return super(ActivityListView, self).get_context_data(**kwargs)
