ac3f5d53faafe1e09a4b29a07365c5c332fa91d1
jalaziz <me@jalaziz.net>
2011-05-22 18:48:26 -0700

    Added the activity detail view and the ability to assign users to activities

--- tectracking/activities/views.py
+++ tectracking/activities/views.py
@@ -35,6 +35,88 @@ class ActivityListView(ListView):
 class ActivityDetailView(DetailView):
     model = Activity
 
+    def get_context_data(self, **kwargs):
+        priority_choices = Assignment.PRIORITY_CHOICES
+        
+        try:
+            assignment = self.object.assignment
+        except Assignment.DoesNotExist:
+            assignment = None
+
+
+        if self.request.user.has_perm('activities.can_accept_activity'):
+            users = User.objects.filter(is_active='Y')
+        else:
+            users = None
+
+        context = {
+            'users': users,
+            'assignment': assignment,
+            'priority_choices': priority_choices,
+        }
+
+        kwargs.update(context)
+        return super(ActivityDetailView, self).get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        errors = []
+        messages = []
+        dotdict = DotExpandedDict(request.POST)
+
+        self.object = self.get_object()
+
+        if self.request.user.is_authenticated():
+            if dotdict.get('assignment', ''):
+                if self.request.user.has_perm('activities.can_accept_activity'):
+                    try:
+                        assignment = Assignment.objects.get(activity=self.object)
+                    except Assignment.DoesNotExist:
+                        assignment = Assignment(activity=self.object)
+
+                    user = dotdict['assignment'].get('user', '')
+                    if user:
+                        try:
+                            assignment.user = User.objects.get(pk=int(user))
+                        except (User.DoesNotExist, ValueError):
+                            errors.append('User with id %s does not exist.' % user)
+                    elif not assignment.user:
+                        errors.append('You must assign a user to an activity.')
+
+                    priority = dotdict['assignment'].get('priority', '')
+                    if priority:
+                        try:
+                            assignment.priority = int(priority)
+                        except ValueError:
+                            errors.append('Priority must be an integer.')
+                    elif not assignment.priority:
+                        errors.append('You must assign a priority to an activity.')
+
+                    escalation_status = dotdict['assignment'].get('escalation_status', '')
+                    if escalation_status:
+                        assignment.escalation_status = escalation_status
+
+                    if not errors:
+                        try:
+                            assignment.full_clean()
+                            assignment.save()
+
+                            if self.object.status == 'N':
+                                self.object.status = 'A'
+                                self.object.save()
+                        except ValidationError, e:
+                            for key, value in e.message_dict:
+                                errors.append(value)
+                else:
+                    errors.append('You do not have permission to assign a user to an activity.')
+        else:
+            errors.append('You must be logged in to edit an activity.')
+
+        if errors:
+            context = self.get_context_data(errors=errors, messages=messages)
+            return self.render_to_response(context)
+        else:
+            return HttpResponseRedirect(self.request.get_full_path())
+
 class ActivityTaskListView(ListView):
     model = ActivityTask
 
