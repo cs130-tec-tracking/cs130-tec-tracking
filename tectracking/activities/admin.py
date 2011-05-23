98579f19305fdbaf6396ad0acf7634d4c463d4d1
jalaziz <me@jalaziz.net>
2011-05-23 01:54:07 -0700

    Activities are now searchable by the tec id and approved id on the activity admin page

--- tectracking/activities/admin.py
+++ tectracking/activities/admin.py
@@ -23,7 +23,7 @@ class NoteInline(admin.TabularInline):
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('tec_id', 'approved_id', 'event_type', 'short_desc', 'event_start_date', 'siebel_num', 'num_attendees',)
     list_filter = ('assignment__user', 'event_type', 'tec_site', 'num_attendees', 'room_required', 'laptops_required',)
-    search_fields = ('assignment__user__first_name', 'assignment__user__last_name', 'siebel_num', 'event_organizer', 'event_type', 'tec_site',)
+    search_fields = ('tec_id', 'approved_id', 'assignment__user__first_name', 'assignment__user__last_name', 'siebel_num', 'event_organizer', 'event_type', 'tec_site',)
     inlines = (AssignmentInline, ActivityTaskInline, NoteInline, RoomReservationInline,)
     date_hierarchy = 'event_start_date'
 
