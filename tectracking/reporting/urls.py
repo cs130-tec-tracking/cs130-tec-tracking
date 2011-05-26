d637ea5529cf5e794e7f56bb0daee74bd997c710
jalaziz <me@jalaziz.net>
2011-05-26 06:29:41 -0700

    Added a tasks reporting view

--- tectracking/reporting/urls.py
+++ tectracking/reporting/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.base import RedirectView
-from tectracking.reporting.views import ActivityReportingView, InventoryReportingView
+from tectracking.reporting.views import ActivityReportingView, InventoryReportingView, TaskReportingView
 from tectracking.utils import reverse_lazy
 
 urlpatterns = patterns(
     '',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('reporting_activity')), name='reporting'),
     url(r'^activities/$', ActivityReportingView.as_view(), name='reporting_activity'),
+    url(r'^tasks/$', TaskReportingView.as_view(), name='reporting_tasks'),
     url(r'^inventory/$', InventoryReportingView.as_view(), name='reporting_inventory'),
 )
--- tectracking/reporting/views.py
+++ tectracking/reporting/views.py
@@ -1,10 +1,14 @@
-from tectracking.activities.models import Activity
+from tectracking.activities.models import Activity, ActivityTask
 from tectracking.inventory.models import Asset
 from django.views.generic.list import ListView
 
 class ActivityReportingView(ListView):
     model = Activity
     template_name = 'reporting/activities.html'
+
+class TaskReportingView(ListView):
+    model = ActivityTask
+    template_name = 'reporting/tasks.html'
 
 class InventoryReportingView(ListView):
     model = Asset
